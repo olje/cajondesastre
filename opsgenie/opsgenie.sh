8dcad2c16817d2b743d4275206d3416e87c30408
olje <jesus.olea@gmail.com>
2023-07-11 09:20:39 +0200

    Update opsgenie.sh
    latest working version

--- opsgenie/opsgenie.sh
+++ opsgenie/opsgenie.sh
@@ -1,148 +1,139 @@
 #!/bin/bash
 
-api_key="YOUR_OPSGENIE_API_KEY"
+api_key=$OPSGENIE_API_KEY
 
-create_alert_url="http://localhost:5000/v2/alerts"
-close_alert_url="http://localhost:5000/v2/alerts/close"
-list_alerts_url="http://localhost:5000/v2/alerts"
+create_alert_url="https://api.eu.opsgenie.com/v2/alerts"
+close_alert_url="https://api.eu.opsgenie.com/v2/alerts/"
+list_alerts_url="https://api.eu.opsgenie.com/v2/alerts"
 
 send_opsgenie_alert() {
 priority="$1"
 message="$2"
 
-bash
-Copy code
 headers=(
-    "Content-Type: application/json"
     "Authorization: GenieKey $api_key"
+)
+
+headercontent=(
+    "Content-type: application/json"
 )
 
 payload=$(cat <<EOF
 {
 "message": "$message",
-"priority": "$priority"
+"priority": "$priority",
+"responders":[
+        {"username":"jesusjavier.olearos@bit.admin.ch", "type":"user"},
+        {"name":"APP-ABN", "type":"team"}
+    ]
 }
 EOF
 )
 
-bash
-Copy code
-response=$(curl -s -X POST -H "${headers[@]}" -d "$payload" "$create_alert_url")
-
-if [[ $(jq -r '.status_code' <<<"$response") == 202 ]]; then
+response=$(curl -X POST -H "$headers" -H "$headercontent" -d "$payload" "$create_alert_url")
+alert_id=$(jq -r '.requestId' <<<"$response")
+if [[ -n $alert_id ]]; then
     echo "Alert sent to Opsgenie successfully!"
-    alert_id=$(jq -r '.data.id' <<<"$response")
-    echo "$alert_id"
+    #echo "response: $response"
+    #echo "$alert_id"
 else
-    echo "Failed to send alert to Opsgenie. Status Code: $(jq -r '.status_code' <<<"$response")"
-    echo "Response: $(jq -r '.text' <<<"$response")"
+    echo "Failed to send alert to Opsgenie. RequestID: $(jq -r '.requestId' <<<"$response")"
+    echo "Response: $response"
+    return 1 
 fi
 }
 
 close_opsgenie_alert() {
-alert_id="$1"
+alertId="$1"
 
-bash
-Copy code
 headers=(
-    "Content-Type: application/json"
     "Authorization: GenieKey $api_key"
 )
 
 payload=$(cat <<EOF
 {
-"id": "$alert_id"
+    "user":"Monitoring Script",
+    "source":"automatic script TODO URL bitbucket",
+    "note":"Action executed via Alert API"
 }
 EOF
 )
 
-bash
-Copy code
-response=$(curl -s -X POST -H "${headers[@]}" -d "$payload" "$close_alert_url")
+response=$(curl -X POST -H "${headers[@]}" -H "Content-Type: application/json" -d "$payload" "$
+close_alert_url/$alertId/close")
 
-if [[ $(jq -r '.status_code' <<<"$response") == 200 ]]; then
+if [[ -z $(jq -r '.requestId' <<<"$response") ]]; then
     echo "Alert closed successfully!"
 else
-    echo "Failed to close alert. Status Code: $(jq -r '.status_code' <<<"$response")"
-    echo "Response: $(jq -r '.text' <<<"$response")"
+    echo "Failed to close alert. Status Code: "$response")"
 fi
 }
 
 get_related_alerts() {
 incident_message="$1"
 
-bash
-Copy code
 headers=(
-    "Content-Type: application/json"
     "Authorization: GenieKey $api_key"
 )
 
 params=(
-    "query=$incident_message"
+    "query=message:%20"$incident_message"%20AND%20status:%20open&limit=10&sort=createdAt&order=
+desc"
 )
 
-response=$(curl -s -X GET -H "${headers[@]}" "$list_alerts_url?${params[*]}")
+response=$(curl -X GET -H "${headers[@]}" "$list_alerts_url?${params[@]}")
 
-if [[ $(jq -r '.status_code' <<<"$response") == 200 ]]; then
-    data=$(jq -r '.data' <<<"$response")
-    echo "$data"
+data=$(jq -r '.data[].id' <<<"$response")
+ 
+if [[ -n $data ]]; then
+  echo "$data"
 else
-    echo "Failed to get related alerts. Status Code: $(jq -r '.status_code' <<<"$response")"
-    echo "Response: $(jq -r '.text' <<<"$response")"
+  echo ""
+  return -1
 fi
 }
 
 print_usage() {
-  echo "Usage: ./script.sh <priority> <message> <incident_still_exists>"
+  echo "Usage: $0 <message> <priority P1-P4 or close>" 
   echo "Parameters:"
-  echo " <priority> Priority of the alert (P1, P2, P3, P4)"
   echo " <message> The incident message to check"
-  echo " <incident_still_exists> Incident existence status (true/false)"
+  echo " <priority> Priority of the alert (P1, P2, P3, P4) or (close) to close the alert"
   exit 1
 }
 
-Validate and retrieve command-line arguments
-if [[ $# != 3 ]]; then
+# Validate and retrieve command-line arguments
+if [[ $# != 2 ]]; then
   print_usage
 fi
 
-priority="$1"
-incident_message="$2"
-incident_still_exists="${3,,}"
+priority="$2"
+incident_message="$1"
 
-Validate incident parameter
-if [[ ! $incident_still_exists =~ ^(true|false)$ ]]; then
-  echo "Invalid value for <incident_still_exists>. Use 'true' or 'false'."
-  echo "Value: $incident_still_exists"
+# Validate priority parameter
+valid_priorities=("P1" "P2" "P3" "P4" "close")
+if [[ ! " ${valid_priorities[@]} " =~ " $priority " ]]; then
+  echo "Invalid value for <priority>. Use one of: ${valid_priorities[@]}"
   print_usage
 fi
 
-Validate priority parameter
-valid_priorities=("P1" "P2" "P3" "P4")
-if [[ ! " ${valid_priorities[]} " =~ " $priority " ]]; then
-  echo "Invalid value for <priority>. Use one of: ${valid_priorities[]}"
-  print_usage
-fi
-
-if [[ $incident_still_exists == "false" ]]; then
 # Check if there are related alerts
- related_alerts=$(get_related_alerts "$incident_message")
-  if [[ -n $related_alerts ]]; then
+related_alerts=$(get_related_alerts "$incident_message")
+
+if [[ $priority == "close" ]]; then
+# Here we check if there are related alerts and if we close them
+  if [ ${#related_alerts[@]} -ne 0 ]; then
   # Close related alerts
   while IFS= read -r alert; do
     close_opsgenie_alert "$alert"
   done <<<"$related_alerts"
-  echo "Resolved incident. Closed related alerts."
+    echo "Resolved incident. Closed related alerts."
   else
-   echo "Resolved incident. No related alerts to close."
+    echo "Resolved incident. No related alerts to close."
   fi
 else
-# Check if there are related alerts
-  related_alerts=$(get_related_alerts "$incident_message")
-  if [[ -n $related_alerts ]]; then
-    echo "Incident still exists. Related alerts already present."
-  else
+ if [[ -n $related_alerts ]]; then
+   echo "Incident still exists. Related alerts already present."
+ else
   # Send a new alert
     send_opsgenie_alert "$priority" "$incident_message"
   fi
